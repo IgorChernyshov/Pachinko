b5686f804f2eadcdc80ec04a544d7b1f169a16c0
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-07-07 22:33:12 +0300

    Initial Commit

---
+++ Pachinko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pachinko
+//
+//  Created by Igor Chernyshov on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
